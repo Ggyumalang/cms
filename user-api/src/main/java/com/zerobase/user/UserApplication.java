7b0c6170dee0650348e7ef94107df3cccc0a9483
Ggyumalang <khg2154@naver.com>
2023-03-24 11:45:07 +0900

    feat : [002-SIGN-UP] 고객에 대한 SignUp Service 구현

--- user-api/src/main/java/com/zerobase/user/UserApplication.java
+++ user-api/src/main/java/com/zerobase/user/UserApplication.java
@@ -1,13 +1,24 @@
 package com.zerobase.user;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
 public class UserApplication {
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:application-mailgun.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class , args);
+        new SpringApplicationBuilder(UserApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
